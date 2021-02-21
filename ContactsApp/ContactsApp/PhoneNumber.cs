71751c48ab31a27469c909df4fd7496133210dd8
ImfeR <imfer99@gmail.com>
2021-02-21 22:41:00 +0700

    Создан интерфейс приложения, класс PhoneNumber и интерфейс ICloneable

---
+++ ContactsApp/ContactsApp/PhoneNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ContactsApp
+{
+    public class PhoneNumber
+    {
+        private string _number;
+
+        public string Number
+        {
+            get { return _number; }
+
+            set
+            {
+                for (var i = 0; i < value.Length; i++)
+                {
+                    if (!char.IsDigit(value, i))
+                    {
+                        throw new ArgumentException("Вводимое значение должно состоять из чисел");
+                    }
+                }
+                if (value.Length != 11)
+                {
+                    throw new ArgumentException(
+                        "Количество чисел в номере телефона должны быть равны 11 вместе с 7, а их было " +
+                        value.Length);
+                }
+
+                if (value[0] != '7')
+                {
+                    throw new ArgumentException("Номер должен начинаться с 7, а вы ввели " + value[0]);
+                }
+                else
+                {
+                    _number = value;
+                }
+            }
+        }
+    }
+}
--- ContactsApp/ContactsAppUI/MainForm.Designer.cs
+++ ContactsApp/ContactsAppUI/MainForm.Designer.cs
@@ -28,20 +28,347 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Find = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.FindBox = new System.Windows.Forms.TextBox();
+            this.SurnameBox = new System.Windows.Forms.TextBox();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.PhoneBox = new System.Windows.Forms.TextBox();
+            this.VKIdBox = new System.Windows.Forms.TextBox();
+            this.EmailBox = new System.Windows.Forms.TextBox();
+            this.BirthdayDateTime = new System.Windows.Forms.DateTimePicker();
+            this.ContactsList = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addContactToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editContactToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeContactToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddContactButton = new System.Windows.Forms.Button();
+            this.ChangeContact = new System.Windows.Forms.Button();
+            this.RemoveContactButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Find
+            // 
+            this.Find.AutoSize = true;
+            this.Find.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Find.Location = new System.Drawing.Point(8, 30);
+            this.Find.Name = "Find";
+            this.Find.Size = new System.Drawing.Size(37, 15);
+            this.Find.TabIndex = 0;
+            this.Find.Text = "Find: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(294, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Suname: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(305, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(302, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(295, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Birthday: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(305, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "vk.com: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(302, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "E-mail: ";
+            // 
+            // FindBox
+            // 
+            this.FindBox.Location = new System.Drawing.Point(51, 30);
+            this.FindBox.Name = "FindBox";
+            this.FindBox.Size = new System.Drawing.Size(219, 20);
+            this.FindBox.TabIndex = 7;
+            // 
+            // SurnameBox
+            // 
+            this.SurnameBox.Location = new System.Drawing.Point(352, 27);
+            this.SurnameBox.Name = "SurnameBox";
+            this.SurnameBox.Size = new System.Drawing.Size(488, 20);
+            this.SurnameBox.TabIndex = 8;
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(352, 53);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(488, 20);
+            this.NameBox.TabIndex = 9;
+            // 
+            // PhoneBox
+            // 
+            this.PhoneBox.Location = new System.Drawing.Point(352, 105);
+            this.PhoneBox.Name = "PhoneBox";
+            this.PhoneBox.Size = new System.Drawing.Size(488, 20);
+            this.PhoneBox.TabIndex = 11;
+            // 
+            // VKIdBox
+            // 
+            this.VKIdBox.Location = new System.Drawing.Point(352, 157);
+            this.VKIdBox.Name = "VKIdBox";
+            this.VKIdBox.Size = new System.Drawing.Size(488, 20);
+            this.VKIdBox.TabIndex = 13;
+            // 
+            // EmailBox
+            // 
+            this.EmailBox.Location = new System.Drawing.Point(352, 133);
+            this.EmailBox.Name = "EmailBox";
+            this.EmailBox.Size = new System.Drawing.Size(488, 20);
+            this.EmailBox.TabIndex = 12;
+            // 
+            // BirthdayDateTime
+            // 
+            this.BirthdayDateTime.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.BirthdayDateTime.Location = new System.Drawing.Point(352, 79);
+            this.BirthdayDateTime.Name = "BirthdayDateTime";
+            this.BirthdayDateTime.Size = new System.Drawing.Size(93, 20);
+            this.BirthdayDateTime.TabIndex = 14;
+            // 
+            // ContactsList
+            // 
+            this.ContactsList.FormattingEnabled = true;
+            this.ContactsList.Location = new System.Drawing.Point(8, 56);
+            this.ContactsList.Name = "ContactsList";
+            this.ContactsList.ScrollAlwaysVisible = true;
+            this.ContactsList.Size = new System.Drawing.Size(262, 381);
+            this.ContactsList.TabIndex = 15;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(852, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addContactToolStripMenuItem,
+            this.editContactToolStripMenuItem,
+            this.removeContactToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // addContactToolStripMenuItem
+            // 
+            this.addContactToolStripMenuItem.Name = "addContactToolStripMenuItem";
+            this.addContactToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addContactToolStripMenuItem.Text = "Add Contact";
+            // 
+            // editContactToolStripMenuItem
+            // 
+            this.editContactToolStripMenuItem.Name = "editContactToolStripMenuItem";
+            this.editContactToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editContactToolStripMenuItem.Text = "Edit Contact";
+            // 
+            // removeContactToolStripMenuItem
+            // 
+            this.removeContactToolStripMenuItem.Name = "removeContactToolStripMenuItem";
+            this.removeContactToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.removeContactToolStripMenuItem.Text = "Remove Contact";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // AddContactButton
+            // 
+            this.AddContactButton.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.AddContactButton.FlatAppearance.BorderSize = 0;
+            this.AddContactButton.FlatAppearance.CheckedBackColor = System.Drawing.Color.WhiteSmoke;
+            this.AddContactButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.WhiteSmoke;
+            this.AddContactButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.AddContactButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddContactButton.Image = ((System.Drawing.Image)(resources.GetObject("AddContactButton.Image")));
+            this.AddContactButton.Location = new System.Drawing.Point(8, 451);
+            this.AddContactButton.Name = "AddContactButton";
+            this.AddContactButton.Size = new System.Drawing.Size(22, 19);
+            this.AddContactButton.TabIndex = 17;
+            this.AddContactButton.UseVisualStyleBackColor = true;
+            // 
+            // ChangeContact
+            // 
+            this.ChangeContact.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ChangeContact.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.ChangeContact.FlatAppearance.BorderSize = 0;
+            this.ChangeContact.FlatAppearance.CheckedBackColor = System.Drawing.Color.WhiteSmoke;
+            this.ChangeContact.FlatAppearance.MouseDownBackColor = System.Drawing.Color.WhiteSmoke;
+            this.ChangeContact.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.ChangeContact.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ChangeContact.Image = ((System.Drawing.Image)(resources.GetObject("ChangeContact.Image")));
+            this.ChangeContact.Location = new System.Drawing.Point(36, 451);
+            this.ChangeContact.Name = "ChangeContact";
+            this.ChangeContact.Size = new System.Drawing.Size(27, 23);
+            this.ChangeContact.TabIndex = 18;
+            this.ChangeContact.UseVisualStyleBackColor = false;
+            // 
+            // RemoveContactButton
+            // 
+            this.RemoveContactButton.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.RemoveContactButton.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.RemoveContactButton.FlatAppearance.BorderSize = 0;
+            this.RemoveContactButton.FlatAppearance.CheckedBackColor = System.Drawing.Color.WhiteSmoke;
+            this.RemoveContactButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.WhiteSmoke;
+            this.RemoveContactButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.WhiteSmoke;
+            this.RemoveContactButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RemoveContactButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RemoveContactButton.Image = ((System.Drawing.Image)(resources.GetObject("RemoveContactButton.Image")));
+            this.RemoveContactButton.Location = new System.Drawing.Point(69, 451);
+            this.RemoveContactButton.Name = "RemoveContactButton";
+            this.RemoveContactButton.Size = new System.Drawing.Size(27, 23);
+            this.RemoveContactButton.TabIndex = 19;
+            this.RemoveContactButton.UseVisualStyleBackColor = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(852, 482);
+            this.Controls.Add(this.RemoveContactButton);
+            this.Controls.Add(this.ChangeContact);
+            this.Controls.Add(this.AddContactButton);
+            this.Controls.Add(this.ContactsList);
+            this.Controls.Add(this.BirthdayDateTime);
+            this.Controls.Add(this.VKIdBox);
+            this.Controls.Add(this.EmailBox);
+            this.Controls.Add(this.PhoneBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.SurnameBox);
+            this.Controls.Add(this.FindBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Find);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "ContactsApp";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Find;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox FindBox;
+        private System.Windows.Forms.TextBox SurnameBox;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox PhoneBox;
+        private System.Windows.Forms.TextBox VKIdBox;
+        private System.Windows.Forms.TextBox EmailBox;
+        private System.Windows.Forms.DateTimePicker BirthdayDateTime;
+        private System.Windows.Forms.ListBox ContactsList;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addContactToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editContactToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeContactToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button AddContactButton;
+        private System.Windows.Forms.Button ChangeContact;
+        private System.Windows.Forms.Button RemoveContactButton;
     }
 }
 
